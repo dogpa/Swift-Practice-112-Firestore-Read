f5cddb03d0712446ee83421146e79a258121727b
Dogpa <dogpa0130@gmail.com>
2021-11-09 20:33:20 +0800

    Initial Commit

---
+++ Swift Practice # 112 Firestore Read/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 112 Firestore Read
+//
+//  Created by Dogpa's MBAir M1 on 2021/11/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
